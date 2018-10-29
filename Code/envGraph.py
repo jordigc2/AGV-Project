1dd5ad6071c64012b16e789e0d9e33f010a1fc27
Jordi.GonzalezCa <jordi.gonzalezca@e-campus.uab.cat>
2018-10-29 16:20:14 +0100

    Calculate dijkstra distances
    from a specific starting node + the distance to that node
    
    Correct the neighbours creation

--- Code/envGraph.py
+++ Code/envGraph.py
@@ -1,5 +1,8 @@
 import sys
 import world as wd
+import bisect
+import math
+import time
 
 
 
@@ -10,14 +13,16 @@ class Node:
 		self.id = nodeId
 		self.neigh = []
 		self.prevNeigh = 0
-		self.statDistance = sys.maxsize #set a huge number to the distances
-		self.dynDistance = sys.maxsize #set a huge number to the distances
+		self.dijkstraDistance = sys.maxsize #set a huge number to the distances
 		self.visited = False
+
+	def __lt__(self, other):
+		return(self.dijkstraDistance < other.dijkstraDistance)
 
 
 class Graph:
 	def __init__(self):
-		self.listOfNodes = []
+		self.listOfNodes = [] #0  WH, 1-3 wallnodes and 4-9 components nodes
 
 		self.world = wd.World()
 
@@ -29,9 +34,8 @@ class Graph:
 			count += 1
 
 		for comp in self.world.availableComp:
-			node = Node(comp.comPos[0], comp.comPos[1], comp.compID+count)
+			node = Node(comp.comPos[0], comp.comPos[1], comp.compID+count-1)
 			self.listOfNodes.append(node)
-			count += 1
 
 		self.setNeighbours()
 
@@ -43,7 +47,7 @@ class Graph:
 				#set warehouse neighbours
 				for aux in self.listOfNodes[1:self.world.numObjects]:
 					node.neigh.append(aux)
-			elif count >= 1 and count <= self.world.numObjects:
+			elif count >= 1 and count < self.world.numObjects:
 				#set wall node neighbours
 				node.neigh.append(self.listOfNodes[0])
 				for aux in self.listOfNodes[self.world.numObjects:len(self.listOfNodes)]:
@@ -55,9 +59,28 @@ class Graph:
 
 				for aux in self.listOfNodes[self.world.numObjects:len(self.listOfNodes)]:
 					node.neigh.append(aux)
+			count += 1
 		print([self.listOfNodes[0].neigh[0].x,self.listOfNodes[0].neigh[0].y])
 
+	def calculateDijkstraDistances(self, startingNodeID, initDistance):
 
+		startNode = self.listOfNodes[startingNodeID]
+		startNode.dijkstraDistance = initDistance
+		priorityQueue = [startNode]
+
+		while len(priorityQueue) > 0:
+			actualNode = priorityQueue.pop(0)
+			if not actualNode.visited:
+
+				for node in actualNode.neigh:
+					dist = actualNode.dijkstraDistance + math.sqrt((node.x-actualNode.x)**2+(node.y-actualNode.y)**2)
+					if dist < node.dijkstraDistance:
+						node.dijkstraDistance = dist
+						node.prevNeigh = actualNode
+					bisect.insort_left(priorityQueue, node)
+
+				actualNode.visited = True
 
 graph = Graph()
 
+graph.calculateDijkstraDistances(0,0)
