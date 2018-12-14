36a2e3e16acb3e67cac70b19af80b1232b84920c
jordigc2 <jordigc2@gmail.com>
2018-12-15 00:11:02 +0100

    security copy of go_to_goal.py, which is working

--- Code/go_to_goal.py
+++ Code/go_to_goal.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import rospy
+from std_msgs.msg import Bool
 from geometry_msgs.msg import TransformStamped, Pose2D
 from AGV5.msg import target				
-from AGV5.msg import robot
+from AGV5.msg import robot, goalPos
 #from tf import transformations as t
 from math import degrees, atan, pi, sqrt
-
 
 def callback(data):
 	global r_x
@@ -25,7 +25,7 @@ def getGoal(data):
 
 	new_goalX = data.x_pos
 	new_goalY = data.y_pos
-	new_mode = data.eul_pos
+	new_mode = data.action
 	print("GOAL X: %f, Y: %f" %(new_goalX, new_goalY) )  
 	print("MOD:" , new_mode)
 
@@ -34,11 +34,12 @@ def calc_angle(cx, cy, ctheta, gx, gy, mode):
 	global goal_reached
 
 	pub = rospy.Publisher("/some/topic", target, queue_size=1000)
+	#pub_log = rospy.Publisher("/wheel/vels", Pose2D, queue_size=1000)
 
-	anglim = 2.0
-	linlim = 0.3
+	anglim = 7.0
+	linlim = 0.5
 
-	kp = 2
+	kp = 3
 	kp_lin = 0.5
 
 	delta_x = gx - cx
@@ -66,7 +67,9 @@ def calc_angle(cx, cy, ctheta, gx, gy, mode):
 		else:
 			angle = vtheta - ctheta  
 
+		#print(degrees(angle))
 		print(vtheta, ctheta, degrees(angle))
+
 #---------------------------Velocity assigment---------------------------------
 		
 
@@ -80,22 +83,22 @@ def calc_angle(cx, cy, ctheta, gx, gy, mode):
 
 		linvel = linlim - (linlim / anglim) * abs(angvel)
 
-#--------------------------------Stopping--------------------------------------
+#--------------------------------Breaking--------------------------------------
 
 		dist = sqrt( delta_x**2 + delta_y**2 )
-
 
 		print("I was called", gx, gy, mode)
 		#print("DISTANCE TO Goal :", dist)
 
-		if  dist < break_length and mode != 0:
-			linvel = dist * kp_lin
 
-		else:
-			linvel = max_linvel
+		if  dist < break_length and linvel > linlim * kp_lin:# and mode == 0:
+			linvel = linlim * kp_lin
 
-		if dist < 0.080:
-			if mode != 0: 
+
+#--------------------------------Stopping--------------------------------------
+
+		if dist < 0.08:
+			if mode != 0 and (abs(gx-cx)<= 0.04): 
 				linvel = 0
 				angvel = 0
 				
@@ -103,7 +106,7 @@ def calc_angle(cx, cy, ctheta, gx, gy, mode):
 				msg.angvel = angvel
 
 				pub.publish(msg)
-				rospy.sleep(5)
+				rospy.sleep(1)
 			goal_reached = True
 
 
@@ -112,10 +115,17 @@ def calc_angle(cx, cy, ctheta, gx, gy, mode):
 
 		pub.publish(msg)
 
-		vr = (2*msg.linvel + msg.angvel*0.125) / (2*0.02) 
-		vl = (2*msg.linvel - msg.angvel*0.125) / (2*0.02)
+		#dat = Pose2D()		
+
+		#dat.x = (2*msg.linvel + msg.angvel*0.125) / (2*0.02) 
+		#dat.y = (2*msg.linvel - msg.angvel*0.125) / (2*0.02)
+		
+		#pub_log.publish(dat)
 		#print("rVel = %f" % vr)
 		#print("lVel = %f" % vl)
+
+
+			
 			
 #------------------------------Main---------------------------------
 
@@ -125,26 +135,26 @@ r_x = 0
 r_y = 0
 r_theta = 0
 
-goalX = 30
-goalY = 30
+goalX = 0
+goalY = 0
 mode = 0
 
-new_goalX = 30
-new_goalY = 30
+new_goalX = 0
+new_goalY = 0
 new_mode = 0
 
-max_linvel = 0.3
-break_length = 0.26
+break_length = 0.3
 
 rospy.init_node("robotController")
 rospy.Subscriber("robotPos", robot, callback)
-gsub = rospy.Subscriber("goalPos", robot, getGoal)
-r = rospy.Rate(10)
+gsub = rospy.Subscriber("goalPos", goalPos, getGoal)
+r = rospy.Rate(25)
 
 rospy.sleep(2)
 
 
 while not rospy.is_shutdown():
+
 	#print ("goal reached, new x and new y : " ,goal_reached, new_goalX, new_goalY)
 	if goal_reached == True and (new_goalX != goalX or new_goalY != goalY):
 		print "NEW GOAL!!!"
